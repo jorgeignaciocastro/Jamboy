9b92edda37bec1f6def1536ac65b7bdd2da49ae2
ignacio castro <ignacio@castro.com>
2017-01-21 06:22:30 -0300

    init pins constants

---
+++ pins.h
@@ -0,0 +1,25 @@
+//TFT PINS
+
+#define TFT_CS     10
+#define TFT_RST    9  
+#define TFT_DC     8
+
+#define TFT_SCLK 13   
+#define TFT_MOSI 11   
+
+// BUTTONS
+
+#define buttonAPin  3
+#define buttonBPin  4
+#define buttonCPin  5
+
+
+// AUDIO
+
+#define audioPin 2
+
+
+// ANALOG JOYSTIC
+
+#define analogY A1
+#define analogX A0
